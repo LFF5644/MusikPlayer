d0c77ef23fdd85706b721fcf780227d15f926cdf
LFF5644 (Code-Server) <meerschweinchen5644@gmail.com>
2023-03-31 21:42:04 +0200

    works with audio-player-lib

--- main.js
+++ main.js
@@ -6,16 +6,13 @@ const {
 	mkdirSync,
 }=require("fs");
 const fetch=require("node-fetch");
-const child_process=require("child_process");
+const audioPlayerLib=require("audio-player-lib");
 
 const config_tracks="tracks.json";
-const config_playback="playback.json";
 const folderDownloads="downloads";
 
 const tracks=JSON.parse(readFileSync(config_tracks,"utf-8"));
-const playback=JSON.parse(readFileSync(config_playback,"utf-8"));
-const canPlay=tracks.files;
-const tracksToPlay=canPlay;	// Creates a new Object
+const player=audioPlayerLib.createPlayer();
 
 function download(){return new Promise(async resolve=>{
 	try{mkdirSync(folderDownloads)}catch(e){} // if "catch" the folder already exist!
@@ -35,8 +32,7 @@ function download(){return new Promise(async resolve=>{
 		const [fileName_utf8,url]=data;
 		const downloaded=downloadFileNames.find(item=>item[1]==fileName_utf8);
 		if(!downloaded){continue;}
-		tracksToPlay.push(folderDownloads+"/"+downloaded[0]);
-		canPlay.push(folderDownloads+"/"+downloaded[0]);
+		player.addTrack(folderDownloads+"/"+downloaded[0]);
 	}
 
 	if(downloadRequired.length==0){
@@ -59,8 +55,7 @@ function download(){return new Promise(async resolve=>{
 			const filePath=folderDownloads+"/"+fileName_hex;
 
 			writeFileSync(filePath,fileData);
-			tracksToPlay.push(filePath);
-			canPlay.push(filePath);
+			player.addTrack(filePath);
 		}
 		catch(e){
 			console.log(`${fileName_utf8} konnte nicht heruntergeladen werden prüfen sie ihre Verbindung!`);
@@ -72,57 +67,10 @@ function download(){return new Promise(async resolve=>{
 	resolve();
 	return;
 })}
-function cmd(cmd,data){return new Promise(async resolve=>{
-	child_process.exec(cmd,(error,log)=>{resolve([error,log])});
-})}
-async function shutdown(){
-	console.log("shutdown...");
-	if(process.platform.startsWith("win")){	// win32; win64
-		await cmd("taskkill -f -im wscript.exe");
-	}
-	else{
-		await cmd("pkill mplayer");
-	}
-	process.exit();
+
+download();
+
+for(const track of tracks.files){
+	player.addTrack(track);
 }
-function playTrack(data){return new Promise(async resolve=>{
-	const {file}=data;
-
-	if(process.platform.startsWith("win")){	// win32; win64
-		writeFileSync("_player.vbs",`\
-			Set Sound=CreateObject("WMPlayer.OCX.7")
-			Sound.URL="${file}"
-			Sound.Controls.play
-			do while Sound.currentmedia.duration=0
-			wscript.sleep 100
-			loop
-			wscript.sleep(int(Sound.currentmedia.duration)+1)*1000\
-		`.split("\t").join(""));
-		await cmd("start \"\" /wait _player.vbs");
-		writeFileSync("_player.vbs","");
-		resolve();
-		return;
-	}
-	else{
-		await cmd(`mplayer "${file}"`);
-		resolve();
-		return;
-	}
-})}
-
-process.on('exit',shutdown);
-process.on('SIGINT',shutdown);
-process.on('SIGUSR1',shutdown);
-process.on('SIGUSR2',shutdown);
-
-(async ()=>{
-	download();
-	let track;
-	while(playback.repeat){
-		for(track of tracksToPlay){
-			console.log("playing",track);
-			await playTrack({file:track});
-		}
-	}
-	console.log("Fertig!")
-})();
+player.play();
